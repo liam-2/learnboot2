7cb7934e9704b68527c3deaa3b1d0df34ceb0d5b
liam <liam@easestrategy.com>
2021-03-09 21:10:00 +0800

    学到31

--- src/main/java/com/atiguigu/boot/controller/ParameterTestController.java
+++ src/main/java/com/atiguigu/boot/controller/ParameterTestController.java
@@ -1,2 +1,70 @@
-package com.atiguigu.boot.controller;public class ParameterTestController {
+package com.atiguigu.boot.controller;
+
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ParameterTestController {
+
+    //  car/2/owner/zhangsan
+    @GetMapping("/car/{id}/owner/{username}")
+    public Map<String,Object> getCar(@PathVariable("id") Integer id,
+                                     @PathVariable("username") String name,
+                                     @PathVariable Map<String,String> pv
+                                     ){
+
+        Map<String,Object> map = new HashMap<>();
+
+       map.put("id",id);
+       map.put("name",name);
+       map.put("pv",pv);
+//        map.put("userAgent",userAgent);
+//        map.put("headers",header);
+        /*map.put("age",age);
+        map.put("inters",inters);
+        map.put("params",params);
+        map.put("_ga",_ga);*/
+       // System.out.println(cookie.getName()+"===>"+cookie);
+        return map;
+    }
+
+
+    @PostMapping("/save")
+    public Map postMethod(@RequestBody String content){
+        Map<String,Object> map = new HashMap<>();
+        map.put("content",content);
+        return map;
+    }
+
+
+    @GetMapping("/cars/{path}")
+    public Map carsSell(@MatrixVariable("low") Integer low,
+                        @MatrixVariable("brand") List<String> brand,
+                        @PathVariable("path") String path){
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("low",low);
+        map.put("brand",brand);
+        map.put("path",path);
+        return map;
+    }
+
+    // /boss/1;age=20/2;age=10
+
+    @GetMapping("/boss/{bossId}/{empId}")
+    public Map boss(@MatrixVariable(value = "age",pathVar = "bossId") Integer bossAge,
+                    @MatrixVariable(value = "age",pathVar = "empId") Integer empAge){
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("bossAge",bossAge);
+        map.put("empAge",empAge);
+        return map;
+
+    }
+
 }
